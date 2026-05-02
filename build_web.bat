e1e70e269abdd61aa3532739a3ac942a2efa25a8
(George)-Yihao-Xu <xuyihao342@gmail.com>
2026-05-02 22:09:16 +0800

    nice build file

--- build_web.bat
+++ build_web.bat
@@ -16,10 +16,10 @@ echo   5. Run this script: .\build_web.bat
 echo.
 echo ============================================
 
-:: Verify emcc is available
-where emcc >nul 2>&1
+:: Verify call emcc is available
+where call emcc >nul 2>&1
 if errorlevel 1 (
-    echo ERROR: emcc not found in PATH.
+    echo ERROR: call emcc not found in PATH.
     echo Please install and activate emsdk first ^(see instructions above^).
     pause
     exit /b 1
@@ -31,39 +31,39 @@ if not exist "docs" mkdir docs
 powershell -Command "(Get-Content 'build/external/raylib-master/src/shell.html') -replace 'raylib web game', '3D Cloth Simulation' | Set-Content 'docs/shell.html'"
 
 set "RAYLIB_SRC=build/external/raylib-master/src"
-set "INCLUDES=-I%RAYLIB_SRC% -I%RAYLIB_SRC%/external -I%RAYLIB_SRC%/external/glfw/include -Iinclude -Isrc -Ibuild/external"
+set "INCLUDES=-I%RAYLIB_SRC% -I%RAYLIB_SRC%/external/glfw/include -Iinclude -Isrc -Ibuild/external"
 set "DEFINES=-DPLATFORM_WEB -DGRAPHICS_API_OPENGL_ES2 -D_GNU_SOURCE"
 set "CFLAGS=-O3 %DEFINES% %INCLUDES% -Wall -Wno-missing-braces"
 set "CXXFLAGS=-O3 %DEFINES% %INCLUDES% -Wall -Wno-missing-braces -std=c++17"
 
 echo [1/3] Compiling raylib...
-emcc -c %RAYLIB_SRC%/rcore.c     -o build/web/rcore.o     %CFLAGS%
+call emcc -c %RAYLIB_SRC%/rcore.c     -o build/web/rcore.o     %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/rshapes.c   -o build/web/rshapes.o   %CFLAGS%
+call emcc -c %RAYLIB_SRC%/rshapes.c   -o build/web/rshapes.o   %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/rtextures.c -o build/web/rtextures.o %CFLAGS%
+call emcc -c %RAYLIB_SRC%/rtextures.c -o build/web/rtextures.o %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/rtext.c     -o build/web/rtext.o     %CFLAGS%
+call emcc -c %RAYLIB_SRC%/rtext.c     -o build/web/rtext.o     %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/utils.c     -o build/web/utils.o     %CFLAGS%
+call emcc -c %RAYLIB_SRC%/utils.c     -o build/web/utils.o     %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/rmodels.c   -o build/web/rmodels.o   %CFLAGS%
+call emcc -c %RAYLIB_SRC%/rmodels.c   -o build/web/rmodels.o   %CFLAGS%
 if errorlevel 1 goto error
-emcc -c %RAYLIB_SRC%/raudio.c    -o build/web/raudio.o    %CFLAGS%
+call emcc -c %RAYLIB_SRC%/raudio.c    -o build/web/raudio.o    %CFLAGS%
 if errorlevel 1 goto error
 
 echo [2/3] Compiling project sources...
-emcc -c src/Mesh.cpp     -o build/web/Mesh.o     %CXXFLAGS%
+call emcc -c src/Mesh.cpp     -o build/web/Mesh.o     %CXXFLAGS%
 if errorlevel 1 goto error
-emcc -c src/Particle.cpp -o build/web/Particle.o %CXXFLAGS%
+call emcc -c src/Particle.cpp -o build/web/Particle.o %CXXFLAGS%
 if errorlevel 1 goto error
-emcc -c src/Spring.cpp   -o build/web/Spring.o   %CXXFLAGS%
+call emcc -c src/Spring.cpp   -o build/web/Spring.o   %CXXFLAGS%
 if errorlevel 1 goto error
-emcc -c src/main.cpp     -o build/web/main.o     %CXXFLAGS%
+call emcc -c src/main.cpp     -o build/web/main.o     %CXXFLAGS%
 if errorlevel 1 goto error
 
 echo [3/3] Linking...
-emcc build/web/*.o -o docs/index.html -s USE_GLFW=3 -s ASYNCIFY=1 -s TOTAL_MEMORY=134217728 -s EXPORTED_RUNTIME_METHODS=[FS] --preload-file config.txt --shell-file docs/shell.html
+call emcc build/web/rcore.o build/web/rshapes.o build/web/rtextures.o build/web/rtext.o build/web/utils.o build/web/rmodels.o build/web/raudio.o build/web/Mesh.o build/web/Particle.o build/web/Spring.o build/web/main.o -o docs/index.html -s USE_GLFW=3 -s ASYNCIFY=1 -s TOTAL_MEMORY=134217728 -s EXPORTED_RUNTIME_METHODS=[FS] --preload-file config.txt --shell-file docs/shell.html
 if errorlevel 1 goto error
 
 echo. > docs\.nojekyll
